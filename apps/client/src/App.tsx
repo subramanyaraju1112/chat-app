30730c43f719a7f21ada585ed9602749cd0a0f77
subramanyaraju1112 <subramanyaraju1112@gmail.com>
2026-07-27 12:36:07 +0530

    updates: client & server emit added

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -3,18 +3,29 @@ import { socket } from "./socket/socket";
 
 function App() {
   useEffect(() => {
-    socket.connect();
 
     socket.on("connect", () => {
       console.log("✅ Connected:", socket.id);
+
+      socket.emit("send_message", {
+        username: "Subramanya",
+        message: "Hello Socket.IO 👋",
+      });
+    });
+
+    socket.on("receive_message", (data) => {
+      console.log("📩 Received:", data);
     });
 
     socket.on("disconnect", () => {
       console.log("❌ Disconnected");
     });
 
+    socket.connect();
+
     return () => {
       socket.off("connect");
+      socket.off("receive_message");
       socket.off("disconnect");
 
       socket.disconnect();
--- apps/server/src/sockets/index.ts
+++ apps/server/src/sockets/index.ts
@@ -12,6 +12,11 @@ export const initializeSocket = (httpServer: HTTPServer) => {
     io.on("connection", (socket) => {
         console.log(`✅ User Connected: ${socket.id}`);
 
+        socket.on("send_message", (data) => {
+            console.log("📩 Message Received:", data);
+            io.emit("receive_message", data);
+        });
+
         socket.on("disconnect", () => {
             console.log(`❌ User Disconnected: ${socket.id}`);
         });
